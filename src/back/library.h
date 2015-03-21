fa9a7495a595886b5d469d6f1d1b09bbae3a05ca
David 'Judah's Shadow' Blue <yudahsshadow@gmx.com>
2015-03-21 16:52:35 -0400

    Updating .gitignore and fixing license issue (must be gpl v2 only due to sword license)

--- src/back/library.h
+++ src/back/library.h
@@ -1,12 +1,11 @@
 ﻿/*
  * BIBISH Is [a] Bible Interactive SHell, a front-end for the SWORD Project
  * inspired by debian's bible package
-//  * Copyright (C) 2015  David "Judah's Shadow" Blue <yudahsshadow@gmx.com>
+ * Copyright (C) 2015  David "Judah's Shadow" Blue <yudahsshadow@gmx.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; version 2 of the License
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- src/front/parser.cpp
+++ src/front/parser.cpp
@@ -1,11 +1,11 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2015  David Blue <email>
+ * BIBISH Is [a] Bible Interactive SHell, a front-end for the SWORD Project
+ * inspired by debian's bible package
+ * Copyright (C) 2015  David "Judah's Shadow" Blue <yudahsshadow@gmx.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; version 2 of the License
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
