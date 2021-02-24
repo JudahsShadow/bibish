1f4ee18cfe0c1ba0958aa06fc99151644bbcccaf
David "Judah's Shadow" Blue <yudahsshadow@gmx.com>
2021-02-24 13:58:07 -0500

    Add an option to back out of pagination without having to reach the end of the text. Useful for read mode.

--- src/front/display.cpp
+++ src/front/display.cpp
@@ -85,12 +85,15 @@ void Display::displayPages(std::list<page> text) {
         numPages = text.size();
         
         std::cout << displayText;
-//         displaySpacer(numLines);
+        displaySpacer(numLines);
         text.pop_front();
         if(numPages > 1) {
             std::string dummy = "";
-            std::cout << "Press enter for next page";
+            std::cout << "Press enter for next page or q to quit: ";
             std::getline(std::cin,dummy);
+            if(dummy == "q") {
+                return;
+            }
             clearScreen();
             displayHeader();
         }
