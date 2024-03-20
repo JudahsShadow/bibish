b012bbca819395057d8498e8fe17c85bb6e67441
David Judah's Shadow Blue <yudahsshadow@gmail.com>
2024-03-20 14:23:38 -0400

    beginnings of glossary class for handling glossary entries

---
+++ src/back/gloss.h
@@ -0,0 +1,40 @@
+/*
+ * BIBISH Is [a] Bible Interactive SHell, a front-end for the SWORD Project
+ * inspired by Debian's bible package
+ * Copyright (C) 2015-2024  David "Judah's Shadow" Blue <yudahsshadow@gmx.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef GLOSS_H
+#define GLOSS_H
+
+//STL Includes
+#include <string>
+
+//SWORD Includes
+#include <swmgr.h>
+
+class Gloss
+{
+public:
+    void setSwordLibrary(sword::SWMgr *swordLib);
+private:
+    sword::SWMgr *swordLibrary;
+    std::string toLang;
+    std::string fromLang;
+};
+
+#endif // GLOSS_H
